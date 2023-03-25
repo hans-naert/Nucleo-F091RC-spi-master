68d06078f9d3243186b75bcb4737baafb7a1fd93
Hans Naert <hans.naert@vives.be>
2023-03-25 16:40:15 +0100

    regenerate with CubeMX

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -55,7 +55,7 @@ static void MX_USART2_UART_Init(void);
 
 /* USER CODE END PFP */
 
-/* Private code ---------------------------------------------------------*/
+/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
